096e36c9d3fecadfd1bc84be949211909c6a39d5
Oliver Gomez <kayserenrique@gmail.com>
2025-01-23 08:29:44 -0400

    registro boton

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -46,6 +46,12 @@
                 </div>
             @endif
 
+            <dir class="mt-4">
+                <a href="" class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 disabled:opacity-50 transition ease-in-out duration-150 w-fulls">
+                    Registro con Discord 
+                </a>
+            </dir>
+
             <div class="flex items-center justify-end mt-4">
                 <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
                     {{ __('Already registered?') }}
