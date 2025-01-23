539f43683ea22e74990d3bc6ab01b11731a5e3e1
Oliver Gomez <kayserenrique@gmail.com>
2025-01-23 09:50:40 -0400

    registro con discord

--- app/Http/Controllers/SocialiteAuthController.php
+++ app/Http/Controllers/SocialiteAuthController.php
@@ -4,19 +4,45 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Laravel\Socialite\Facades\Socialite;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+
 
 class SocialiteAuthController extends Controller
 {
     public function redirect()
     {
-        return Socialite::driver('github')->redirect();
+        return Socialite::driver('discord')->redirect();
     }
 
     public function callback()
     {
-        $user = Socialite::driver('github')->user();
- 
-        dd($user);
+        $providerUser = Socialite::driver('discord')->user();
+        $user = User::where('email', $providerUser->getEmail())->first();
+        
+        //comprobar si el usuario está registrado
+        if (!$user) {
+            $user = User::create([
+                'email' => $providerUser->getEmail(),
+                'name' => $providerUser->getName(),
+            ]);
+        }
+
+        $user->authProviders()->updateOrCreate([
+            'provider' => 'discord',
+        ], [
+            'provider_id' => $providerUser->getId(),
+            'avatar' => $providerUser->getAvatar(),
+            'token' => $providerUser->token,
+            'nickname' => $providerUser->getNickname(),
+            'login_at' => Carbon::now(),
+        ]);
+
+        Auth::login($user);
+
+        return redirect('/dashboard');
+        
     }
 
 }
